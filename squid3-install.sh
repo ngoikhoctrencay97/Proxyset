8251e7ce4e1daad07a28854761767b5c46ebbe6c
Hoang Nguyen <31663074+ngoikhoctrencay97@users.noreply.github.com>
2023-02-14 03:23:42 +0700

    Update squid3-install.sh

--- squid3-install.sh
+++ squid3-install.sh
@@ -2,9 +2,21 @@
 
 # Squid Installer
 
-sudo apt install squid -y
-/bin/rm -f /etc/squid/squid.conf
-/usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/ngoikhoctrencay97/Proxyset/main/squid.conf
-sudo systemctl restart squid
+if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 20.04"; then
+    /usr/bin/apt update
+    /usr/bin/apt -y install apache2-utils squid3
+    touch /etc/squid/passwd
+    /bin/rm -f /etc/squid/squid.conf
+    /usr/bin/touch /etc/squid/blacklist.acl
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/ngoikhoctrencay97/Proxyset/main/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+    /sbin/iptables-save
+    service squid restart
+    systemctl enable squid
+else
+    echo "OS NOT SUPPORTED.\n"
+    echo "Contact us to add support for your os."
+    exit 1;
+fi
 
-#Heisenberg
+#/usr/bin/htpasswd -b -c /etc/squid/passwd USERNAME_HERE PASSWORD_HERE
